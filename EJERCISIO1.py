42689c2d0aecd98ccf5dc7b6d86554b436c55568
eban798 <108815655+eban798@users.noreply.github.com>
2022-08-03 11:13:52 -0300

    Create EJERCISIO1.py

---
+++ EJERCISIO1.py
@@ -0,0 +1,6 @@
+print("Ingresar la velocidad y el tiempo de la unidad movil")
+V = float(input("velocidad: "))
+T = int(input("tiempo: ")) 
+
+D = V*T
+print("La distancia recorrida por la unidad movil") (D)
